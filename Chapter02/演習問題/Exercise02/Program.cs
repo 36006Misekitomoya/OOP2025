e3cc571d07113bef10ee0eaf6beebac6b89c4fcf
Misekitomoya <oibc36006@ojs.ac.jp>
2025-04-23 10:25:01 +0900

    追加機能①

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -2,29 +2,52 @@
     internal class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("*** 変換アプリ ***");
+            Console.WriteLine("1:インチからメートル");
+            Console.WriteLine("2:メートルからインチ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a == 1) {
+                Console.Write("はじめ:");
+                int start = int.Parse(Console.ReadLine());
+                Console.Write("おわり:");
+                int end = int.Parse(Console.ReadLine());
+                PrintInchToMeterList(start, end);
+            } else if(a == 2) {
+                Console.Write("はじめ:");
+                int start = int.Parse(Console.ReadLine());
+                Console.Write("おわり:");
+                int end = int.Parse(Console.ReadLine());
+                PrintMeterToInchList(start, end);
+            } else {
+                Console.WriteLine("エラー");
+            }
+
+        }
+        static void PrintInchToMeterList(int start, int end) {
             //インチからメートル                              
-            for (int inch = 1; inch <= 10; inch++) {
-                // double meter = feet * 0.3048;                    
+            for (int inch = start; inch <= end; inch++) {
                 double meter = InchToMeter(inch);
                 Console.WriteLine($"{inch}inch = {meter:0.0000}m");
             }
+        }
+         static void PrintMeterToInchList(int start, int end) {
             //メートルからインチ                              
-            for (int meter = 1; meter <= 10; meter++) {
-                // double meter = feet * 0.3048;                    
+            for (int meter = start; meter <= end; meter++) {
                 double inch = MeterToInch(meter);
                 Console.WriteLine($"{meter}m = {inch:0.0000}inch");
 
             }
         }
-
-        //フィートからメートルを求める
+        //インチからメートルを求める
         public static double InchToMeter(int inch) {
             return inch * 0.0254;
         }
-        //フィートからメートルを求める
+        //メートルからインチを求める
         public static double MeterToInch(int meter) {
             return meter / 0.0254;
         }
     }
 }
+
 
