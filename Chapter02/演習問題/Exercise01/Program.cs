46c509a47136a6fe5cc94652a1d5d65e79eb897b
Misekitomoya <oibc36006@ojs.ac.jp>
2025-05-13 10:30:01 +0900

    2.1.1と2.1.2　できた

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[] {
+                new Song()
+                
+            }
+
+
+
+
+
+
+
+        }
+
+            //2.1.4
+           private static void printSongs(Song[] songs) {
+
+        }
+        }
+    }
+
