6f9dd12db98291402f515fd99e7a71079c619632
Misekitomoya <oibc36006@ojs.ac.jp>
2025-05-23 15:32:24 +0900

    4.2.1 完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -11,7 +11,24 @@ namespace Exercise02 {
         }
 
         private static void Exercise1() {
-            
+            var gou = Console.ReadLine();
+            int number;
+            if (int.TryParse(gou, out number)) {
+                if (number < 0) {
+                    Console.WriteLine("変換後: "+number);
+                } else if ( number < 100) {
+                    Console.WriteLine("変換後: "+number * 2);
+                } else if ( number < 500) {
+                    Console.WriteLine("変換後: "+number * 3);
+                } else {
+                    Console.WriteLine("変換後: "+number);
+                }
+                
+                //        Console.WriteLine("変換成功: " + number);
+                //} else {
+                //    Console.WriteLine("変換失敗");
+                //}
+            }
         }
 
         private static void Exercise2() {
