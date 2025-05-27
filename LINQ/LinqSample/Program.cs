da52536507f14284abf71e12b0fb91350cc9d163
Misekitomoya <oibc36006@ojs.ac.jp>
2025-05-27 11:25:35 +0900

    7.1.1 ~ 7.1.5

--- LINQ/LinqSample/Program.cs
+++ LINQ/LinqSample/Program.cs
@@ -2,27 +2,27 @@
     internal class Program {
         static void Main(string[] args) {
 
-            var numbers = Enumerable.Range(1, 10);
+            var numbers = Enumerable.Range(1, 100);
 
             //合計値を出力
-            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
 
-            //偶数の合計を出力            
-            Console.WriteLine(numbers.Where(n => n % 2 == 0).Sum());
+            ////偶数の合計を出力            
+            //Console.WriteLine(numbers.Where(n => n % 2 == 0).Sum());
 
-            //偶数の一番大きい値を出力
-            Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
+            ////偶数の一番大きい値を出力
+            //Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
 
-            //偶数の一番小さい値を出力
-            Console.WriteLine(numbers.Where(n => n % 2 == 0).Min());
+            ////偶数の一番小さい値を出力
+            //Console.WriteLine(numbers.Where(n => n % 2 == 0).Min());
 
 
-            //平均を出力
-            Console.WriteLine(numbers.Average());
+            ////平均を出力
+            //Console.WriteLine(numbers.Average());
 
-            foreach(var num in numbers) {
-                Console.WriteLine(num);
-            }
+            //foreach(var num in numbers) {
+            //    Console.WriteLine(num);
+            //}
         }
     }
 }
