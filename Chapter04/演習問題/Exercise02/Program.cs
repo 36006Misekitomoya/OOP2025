f23c4ca7e97c6deee00fdcb7b7bcaa5f2e0e301b
Misekitomoya <oibc36006@ojs.ac.jp>
2025-05-27 09:32:44 +0900

    lesson4 完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -11,32 +11,57 @@ namespace Exercise02 {
         }
 
         private static void Exercise1() {
-            var gou = Console.ReadLine();
-            int number;
-            if (int.TryParse(gou, out number)) {
-                if (number < 0) {
-                    Console.WriteLine("変換後: "+number);
-                } else if ( number < 100) {
-                    Console.WriteLine("変換後: "+number * 2);
-                } else if ( number < 500) {
-                    Console.WriteLine("変換後: "+number * 3);
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                if (num < 0) {
+                    Console.WriteLine(num);
+                } else if (num < 100) {
+                    Console.WriteLine(num * 2);
+                } else if (num < 500) {
+                    Console.WriteLine(num * 3);
                 } else {
-                    Console.WriteLine("変換後: "+number);
+                    Console.WriteLine(num);
                 }
-                
-                //        Console.WriteLine("変換成功: " + number);
-                //} else {
-                //    Console.WriteLine("変換失敗");
-                //}
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
             }
         }
 
         private static void Exercise2() {
-            
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                switch (num) {
+                    case < 0:
+                        Console.WriteLine(num);
+                        break;
+                    case < 100:
+                        Console.WriteLine(num * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(num * 3);
+                        break;
+                    default:
+                        Console.WriteLine(num);
+                        break;
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
 
         private static void Exercise3() {
-            
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                var text = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num * 3,
+                    _ => num
+                };
+                Console.WriteLine(text);
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
     }
 }
---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 10);
+
+            //合計値を出力
+            Console.WriteLine(numbers.Sum());
+
+            //偶数の合計を出力            
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Sum());
+
+            //偶数の一番大きい値を出力
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
+
+            //偶数の一番小さい値を出力
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Min());
+
+
+            //平均を出力
+            Console.WriteLine(numbers.Average());
+
+            foreach(var num in numbers) {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
