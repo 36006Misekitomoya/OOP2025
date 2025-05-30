d01a149ec226a639ce44856c113d0dbfddaf583f
Misekitomoya <oibc36006@ojs.ac.jp>
2025-05-30 15:10:31 +0900

    6.3.1 6.3.2 完成

--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -3,12 +3,14 @@
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var str1 = "JSON";
-            var str2 = "ＪＳＯＮ";
 
-            var cultureinfo = new CultureInfo("ja-JP");
-            if(String.Compare(str1,str2,cultureinfo,CompareOptions.IgnoreWidth | CompareOptions.IgnoreCase) == 0) {
-                Console.WriteLine("一致しています");
+            var target = "C# Programming";
+            var isExists = target.All(c => Char.IsLower(c));
+           
+
+
+
+
             }
 
 
