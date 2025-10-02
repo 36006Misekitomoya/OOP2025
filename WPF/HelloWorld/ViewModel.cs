632c91655da67b292d1b47f976ee3ceb74610329
Misekitomoya <oibc36006@ojs.ac.jp>
2025-10-02 15:59:43 +0900

    enabled

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -11,14 +11,21 @@ namespace HelloWorld
     {
         public ViewModel() {
             ChangeMessageCommand = new DelegateCommand<string>(
-                (par) => GreetingMessage = par);
+                (par) => GreetingMessage = par,
+                (par) => GreetingMessage != par)
+                .ObservesProperty(() => GreetingMessage);
         }
 
         private string _greetingMessage = "Hello World!";
-        public string GreetingMessage { 
-            get=>_greetingMessage;
-            set => SetProperty(ref _greetingMessage, value);                
+        public string GreetingMessage {
+            get => _greetingMessage;
+            set => SetProperty(ref _greetingMessage, value);                                                          
         }
+        //private bool _canChangedMessage = true;
+        //public bool CanChangedMessage {
+        //    get => _canChangedMessage;
+        //    private set => SetProperty(ref _canChangedMessage, value);
+        //}
 
         public string NewMessage1 { get; } = "ばいばいワールド";
         public string NewMessage2 { get; } = "こんにちワールド";
