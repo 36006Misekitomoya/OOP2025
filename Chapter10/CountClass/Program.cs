8a7b2f043044e95540dba35f70d99d966323d4d9
Misekitomoya <oibc36006@ojs.ac.jp>
2025-11-10 15:16:55 +0900

    chapter10

---
+++ Chapter10/CountClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("読み込むC#ファイル名を入力してください: ");
+        string filePath = Console.ReadLine();
+
+        if (!File.Exists(filePath)) {
+            Console.WriteLine("ファイルが見つかりません。");
+            return;
+        }
+        var lines = File.ReadAllLines(filePath);
+        int count = 0;
+        foreach (var line in lines) {
+            if (line.Contains("class")) {
+                count++;
+            }
+        }
+        Console.WriteLine($"'class' は {count} 回出現しました。");
+    }
+}
---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+
+            hc.DeleteFromJsonAsync
+            await GetHtmlExample(hc)^^\ ;
+        }
+
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://www.yahoo.co.jp/";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
