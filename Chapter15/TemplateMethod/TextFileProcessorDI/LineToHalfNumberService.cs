1114bea61564b5a369be16fbab6deb89a13a02e8
Misekitomoya <oibc36006@ojs.ac.jp>
2025-11-17 15:57:45 +0900

    15.1完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextFileProcessorDI {
+    // P362 問題15.1
+    public class LineToHalfNumberService : ITextFileService {      
+        private List<string> _lines = new List<string>();
+        public void Initialize(string fname) {            
+            _lines.Clear();
+        }
+
+        public void Execute(string line) {           
+            string converted = ConvertToHalfNumbers(line);           
+            _lines.Add(converted);
+        }
+
+        public void Terminate() {           
+            foreach (var line in _lines) {
+                Console.WriteLine(line);
+            }
+        }
+
+        // 全角数字を半角数字に変換するメソッド
+        private string ConvertToHalfNumbers(string text) {          
+            char ConvertChar(char c) {
+                if (c >= '０' && c <= '９') {
+                    return (char)(c - 0xFEE0);
+                }
+                return c;
+            }
+
+            char[] chars = text.ToCharArray();
+            for (int i = 0; i < chars.Length; i++) {
+                chars[i] = ConvertChar(chars[i]);
+            }
+            return new string(chars);
+        }
+    }
+}
--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineOutputService();
+            var service = new LineToHalfNumberService();
+            //var service = new LineCounterService();
+            //var service = new LineOutputService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力");
             String path = Console.ReadLine();
