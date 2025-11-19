ade344547c9f58afdf0ef6fa29f2da9b14420d2f
Misekitomoya <oibc36006@ojs.ac.jp>
2025-11-19 09:42:14 +0900

    15.1完成　二個目

--- Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -1,39 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TextFileProcessorDI {
-    // P362 問題15.1
-    public class LineToHalfNumberService : ITextFileService {      
-        private List<string> _lines = new List<string>();
-        public void Initialize(string fname) {            
-            _lines.Clear();
+    //問題15.1
+    internal class LineToHalfNumberService : ITextFileService {
+        private int _count;
+
+        public void Initialize(string fname) {
         }
 
-        public void Execute(string line) {           
-            string converted = ConvertToHalfNumbers(line);           
-            _lines.Add(converted);
+        public void Execute(string line) {
+            Console.WriteLine(line.Normalize(NormalizationForm.FormKD));
         }
 
-        public void Terminate() {           
-            foreach (var line in _lines) {
-                Console.WriteLine(line);
-            }
-        }
-
-        // 全角数字を半角数字に変換するメソッド
-        private string ConvertToHalfNumbers(string text) {          
-            char ConvertChar(char c) {
-                if (c >= '０' && c <= '９') {
-                    return (char)(c - 0xFEE0);
-                }
-                return c;
-            }
-
-            char[] chars = text.ToCharArray();
-            for (int i = 0; i < chars.Length; i++) {
-                chars[i] = ConvertChar(chars[i]);
-            }
-            return new string(chars);
+        public void Terminate() {
+            Console.WriteLine("変換終了");
         }
     }
 }
+
+        //// 全角数字を半角数字に変換するメソッド
+        //private string ConvertToHalfNumbers(string text) {          
+        //            char ConvertChar(char c) {
+        //                if (c >= '０' && c <= '９') {
+        //                    return (char)(c - 0xFEE0);
+        //                }
+        //                return c;
+        //            }
+
+        //            char[] chars = text.ToCharArray();
+        //            for (int i = 0; i < chars.Length; i++) {
+        //                chars[i] = ConvertChar(chars[i]);
+        //            }
+        //            return new string(chars);
+        //        }
+        //    }
+        //}
